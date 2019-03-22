32db2c149872987b4ddf0f5337fd6ca35b9cdec9
reinethernal <35932698+reinethernal@users.noreply.github.com>
2019-03-22 14:43:46 +0300

    Added option to log user interactions with bot

--- bot.py
+++ bot.py
@@ -9,18 +9,35 @@ import datetime
 import glob
 from subprocess import Popen, PIPE
 from telegram.ext import CommandHandler
-'''модуль для перезагрузки (обновления) других модулей'''
+#'''модуль для перезагрузки (обновления) других модулей'''
 from imp import reload
+import codecs
 # bot = telegram.Bot(token = config.token)
 # Проверка бота
 # print(bot.getMe())
+import logging
+from datetime import datetime
+
+
+
 
 from telegram.ext import Updater
 updater = Updater(token=config.token)
 dispatcher = updater.dispatcher
 
+# Enable logging
+#logging.basicConfig(filename='/home/tgbot/bot.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+#logger = logging.getLogger(__name__)
 
 #Users = ['ReinEthernal']
+
+logfile = "/home/tgbot/bot2.log"
+
+def logString(s):
+    f=open(logfile, "at")
+    f.write( datetime.now().strftime("%d.%m.%y %H:%M:%S ")+  s +'\n')
+    f.close()
+
 
 
 def checkUser(bot,update):
@@ -45,20 +62,24 @@ def help(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text='Введите /getpass* чтобы получить пароль.')
 
 def gp4(pcname):
-    path="/home/zadmin/4/passwords/"
-    with open(path+pcname+'.pass', 'r') as f:
-          return f.read()
+    path="/mnt/passes/"
+    with codecs.open(path+pcname+'.pass',encoding='cp866') as f:
+       l=f.read().strip()
+       return l [ l.find(':')+2:  ]
 
 
 def getpass(bot, update):
-    print('getpasss....')
+    print('getpass....')
     if not checkUser(bot, update): 
        print('not checked..')
+       logString('bad attempt for user '+update.message.from_user.username)
        return
     else:
        print('checked...')
 
     msg = update.message.text.split(' ')[1]
+    print('msg: '+msg)
+    logString('sending pass for pc '+msg+' to user ' +update.message.from_user.username)
     bot.send_message(chat_id=update.message.chat_id, text=gp4(msg))
 
 
@@ -68,6 +89,10 @@ dispatcher.add_handler(CommandHandler('getpass', getpass))
 dispatcher.add_handler(CommandHandler('start', start))
 
 
+
+#print( gp4 ("462-0"));
+
+#print(gp4('100-3'))
 
 updater.start_polling()
 
